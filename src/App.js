ffe77704ce28f3fb5d97851ee5596c061ec2ee5c
Aaron Luing <aaronluing@Aarons-MacBook-Air.local>
2020-11-18 12:29:56 -0600

    working on the about section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
-<<<<<<< HEAD:farm_stan/src/App.js
-import About from './components/About'
-=======
 import './App.css'
->>>>>>> 2f4d08487935e3b2dc17ad839282570e425d3459:src/App.js
+import About from './components/About'
 import Footer from './components/Footer'
 import Header from './components/Header'
 
@@ -12,13 +9,9 @@ export default class App extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD:farm_stan/src/App.js
-        <h1><b>FARM STAN</b></h1>
-        <About />
-=======
         <Header />
+        <About />
         <p>index of farms here</p>
->>>>>>> 2f4d08487935e3b2dc17ad839282570e425d3459:src/App.js
         <Footer />
       </div>
     )

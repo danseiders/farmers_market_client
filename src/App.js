a31c652ad854c83a3c8684b661778d0e03d6cb8f
Spencer Haugh <spencer.haugh@gmail.com>
2020-11-18 11:17:22 -0500

    header component built, title and login components built

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import Footer from './components/Footer'
+import Header from './components/Header'
 
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>FARM STAN</h1>
-        <p>axios is added</p>
+        <Header />
         <Footer />
       </div>
     )
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Login from './Login'
+import Title from './Title'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className='Header'>
+                <Title />
+                <Login />
+                <p>axios is added</p>
+            </div>
+        )
+    }
+}

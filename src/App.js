70f56f5d0fe87c3dc484dee25158ecc75ac3d479
Aaron Luing <aaronluing@Aarons-MacBook-Air.local>
2020-11-19 15:16:54 -0600

    implemented Readct Router to the App.js and established introductory functionality

--- src/App.js
+++ src/App.js
@@ -4,19 +4,48 @@ import About from './components/About'
 import Footer from './components/Footer'
 import Groceries from './Pages/Groceries'
 import Header from './components/Header'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="site-body">
-        <Header />
-        <About />
-        <p>index of farms here</p>
-        <Groceries />
-        <Footer />
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <Router>
+      <Header />
+      <Link to="/">Home</Link>
+      <Link to="/groceries">Vegetables</Link>
+      <Switch>
+        <Route path="/groceries">
+          <Vegies />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
+
+function Home() {
+  return <About />
+}
+
+function Vegies() {
+  return <Groceries />
+}
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <Router>
+//         <div className="site-body">
+//           <Header />
+//           <About />
+//           <p>index of farms here</p>
+//           <Route path='/groceries' component={Groceries} />
+//           <Footer />
+//         </div>
+//       </Router>
+//     )
+//   }
+// }
 

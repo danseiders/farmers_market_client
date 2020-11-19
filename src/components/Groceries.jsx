a2a509daa90c3b40448699bc09c29e695ee2cd19
Aaron Luing <aaronluing@Aarons-MacBook-Air.local>
2020-11-18 18:04:27 -0600

    axios get request working with demo api

---
+++ src/components/Groceries.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class Groceries extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            vegetables: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`http://jsonplaceholder.typicode.com/users`)
+        .then(res => {
+            console.log(res)
+            this.setState({ vegetables: res.data })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>VEGETABLES:</h1>
+                <ul>
+                {this.state.vegetables.map((vegie) => 
+                <li>{vegie.name}</li>
+                )}
+                </ul>
+            </div>
+        )
+    }
+}

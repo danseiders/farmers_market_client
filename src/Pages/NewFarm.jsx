f4783fed1846d06d44923acbd31e0fd00540a093
Daniel Seiders <DanSeiders@dseid.lan>
2020-11-20 21:18:54 -0500

    login posting to users, token is being returned

--- src/Pages/NewFarm.jsx
+++ src/Pages/NewFarm.jsx
@@ -1,5 +1,5 @@
-import axios from 'axios'
 import React, { Component } from 'react'
+import axios from 'axios'
 import { Redirect } from 'react-router'
 
 export default class NewFarm extends Component {
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,60 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { Redirect } from 'react-router'
 
 export default class Login extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+    handleChange = (event) => {
+        this.setState({[event.target.id]: event.target.value })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post('https://farm-stan-api.herokuapp.com/users/login', {
+            email: this.state.email,
+            password: this.state.password
+        })
+        .then(res =>  {
+            console.log(res) 
+        })
+        .catch(err => {
+            console.log(err)
+        })
+        .then(() => {
+            // if(){
+
+            // } else 
+            this.setState({
+                redirect: true,
+            })
+        })
+    }
+    
     render() {
         return (
-            <div className="Login">
+            <form className="Login" onSubmit={this.handleSubmit}>
                 <h4>Log in</h4>
-                <label htmlFor='name'>Username: </label>
-                    <input type="text" name='name'/><br />
+                <label htmlFor='email'>Email: </label>
+                    <input 
+                        type="text" 
+                        name='email'
+                        id='email'
+                        onChange={this.handleChange}/>
+                        <br/>
                 <label htmlFor="password">Password: </label>
-                    <input type='password' name='password'></input><br />
-                <button value="Submit">Submit</button>
-            </div>
+                    <input 
+                        type='password' 
+                        name='password' 
+                        id='password'
+                        onChange={this.handleChange}/>
+                        <br/>
+                <input type="submit" value="Log In"/>
+            </form>
         )
     }
 }

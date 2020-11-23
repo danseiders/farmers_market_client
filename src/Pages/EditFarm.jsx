2beb1ff1da667ab437a048e07e8049157a8ea562
Aaron Luing <aaronluing@Aarons-MacBook-Air.local>
2020-11-23 14:20:33 -0600

    form will populate with user data

--- src/Pages/EditFarm.jsx
+++ src/Pages/EditFarm.jsx
@@ -14,67 +14,110 @@ export default class EditFarm extends Component {
             market: '',
             redirect: null
         }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleDelete = this.handleDelete.bind(this)
     }
 
     componentDidMount() {
-        axios.get(`https://farm-stan-api.herokuapp.com/farms`)
+        const token = sessionStorage.userAuthToken
+        console.log()
+        axios.get('https://farm-stan-api.herokuapp.com/users', {
+            headers: {
+                Authorization: 'bearer ' + token,
+            }
+        })
         .then(res => {
+            const user = res.data.user
             console.log(res)
             this.setState({
+                displayName: user.displayName,
+                address: user.address,
+                email: user.email,
+                market: user.market,
+                phoneNumber: user.phoneNumber,
+                items: user.items
+            })
+        })
+    }
+    handleChange(event){
+        this.setState({ [event.target.id]: event.target.value})
+    }
 
+    handleSubmit(event){
+        event.preventDefault()
+        axios.post(`https://farm-stan-api.herokuapp.com/users`, {
+            email: '',
+            password: '',
+            displayName: '',
+            address: '',
+            phoneNumber: '',
+            items: '',
+            market: '',
+        }).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        }).then(() => {
+            this.setState({
+                redirect: true
             })
         })
     }
 
+    handleDelete(event){
+        axios.delete(`https://farm-stan-api.herokuapp.com/users`)
+    }
+
     render() {
         return (
-            <div>
+            <div className='editFarm'>
                 <h1>Edit Farm</h1>
-                <form onSubmit={this.handleSubmit}>
+                <form className='edit-form' onSubmit={this.handleSubmit} onDelete={this.handleDelete}>
                     <label htmlFor='email'>E-mail:</label>
                     <input
                         type='text'
                         name='email'
                         id='email'
-                        onChange={this.handleChange}/><br/>
-                    <label htmlFor='password'>Password:</label>
-                    <input
-                        type='text'
-                        name='password'
-                        id='password'
+                        defaultValue={this.state.email}
                         onChange={this.handleChange}/><br/>
                     <label htmlFor='displayName'>Username:</label>
                     <input
                         type='text'
                         name='displayName'
                         id='displayName'
-                        value={sessionStorage.displayName}
+                        defaultValue={this.state.displayName}
                         onChange={this.handleChange}/><br/>
                     <label htmlFor='address'>Address:</label>
                     <input
                         type='text'
                         name='address'
                         id='address'
+                        defaultValue={this.state.address}
                         onChange={this.handleChange}/><br/>
                     <label htmlFor='phoneNumber'>Phone Number:</label>
                     <input
                         type='text'
                         name='phoneNumber'
                         id='phoneNumber'
+                        defaultValue={this.state.phoneNumber}
                         onChange={this.handleChange}/><br/>
                     <label htmlFor='items'>Grocery Items:</label>
                     <input
                         type='text'
                         name='items'
                         id='items'
+                        defaultValue={this.state.items}
                         onChange={this.handleChange}/><br/>
                     <label htmlFor='market'> Market:</label>
                     <input
                         type='text'
                         name='market'
                         id='market'
+                        defaultValue={this.state.market}
                         onChange={this.handleChange}/><br/>
                     <button id='submit' value='submit'>Submit</button>
+                    <button id='delete' value='delete'>Delete User</button>
                 </form>
             </div>
         )
